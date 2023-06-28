dea5301b48ea6001c22fb67ed171a9d3a82f438a
Mark Settarov <2top4live@gmail.com>
2023-06-28 13:26:42 +0300

    Separation of concerns: Moved SQL related stuff (query strings) into a separate sql.py module to make the code cleaner and more maintainable.
    Code Duplication: Refactored json.dumps/json.loads into separate helper methods to avoid duplicating the same logic and to improve code readability.

--- database_handler/database_handler.py
+++ database_handler/database_handler.py
@@ -1,8 +1,11 @@
 import json
 import sqlite3
-from typing import List
+from typing import List, Optional
 
 from models.news import News, Interpretation
+
+# moved SQL related stuff to its own module.
+from sql import NEWS_TABLE_SQL, INSERT_NEWS_SQL, SELECT_ALL_NEWS_SQL
 
 
 class DatabaseHandler:
@@ -10,66 +13,61 @@ class DatabaseHandler:
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
 
-        self.cursor.execute("""
-            CREATE TABLE IF NOT EXISTS news (
-                url TEXT,
-                date TEXT,
-                website TEXT,
-                source TEXT,
-                title TEXT,
-                text TEXT,
-                currencies TEXT,
-                hashtags TEXT,
-                interpretation TEXT,
-                reason TEXT,
-                trade_executed BOOLEAN
-            )
-        """)
+        self.cursor.execute(NEWS_TABLE_SQL)
 
     def save_news(self, news_list: List[News]):
         for news in news_list:
-            self.insert_news(news)
+            self._insert_news(news)
 
-    def insert_news(self, news: News):
-        self.cursor.execute("""
-            INSERT INTO news VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, (
+    def _insert_news(self, news: News):
+        self.cursor.execute(INSERT_NEWS_SQL, self._to_db_record(news))
+        self.conn.commit()
+
+    def get_all_news(self):
+        self.cursor.execute(SELECT_ALL_NEWS_SQL)
+        return [self._to_news(record) for record in self.cursor.fetchall()]
+
+    def close(self):
+        self.conn.close()
+
+    # Refactoring: moved record to News and News to record conversion to their own methods.
+    @staticmethod
+    def _to_db_record(news: News) -> tuple:
+        return (
             news.url,
             news.date,
             news.website,
             news.source,
             news.title,
             news.text,
-            json.dumps(news.currencies) if news.currencies is not None else None,
-            json.dumps(news.hashtags) if news.hashtags is not None else None,
+            DatabaseHandler._json_dumps(news.currencies),
+            DatabaseHandler._json_dumps(news.hashtags),
             news.interpretation.value if news.interpretation is not None else None,
             news.reason,
             news.trade_executed
-        ))
-        self.conn.commit()
+        )
 
-    def get_all_news(self):
-        self.cursor.execute("""
-            SELECT * FROM news
-        """)
-        rows = self.cursor.fetchall()
-        news_list = []
-        for row in rows:
-            news = News(
-                url=row[0],
-                date=row[1],
-                website=row[2],
-                source=row[3],
-                title=row[4],
-                text=row[5],
-                currencies=json.loads(row[6]) if row[6] is not None else None,
-                hashtags=json.loads(row[7]) if row[7] is not None else None,
-                interpretation=Interpretation(row[8]) if row[8] is not None else None,
-                reason=row[9],
-                trade_executed=row[10]
-            )
-            news_list.append(news)
-        return news_list
+    @staticmethod
+    def _to_news(record: tuple) -> News:
+        return News(
+            url=record[0],
+            date=record[1],
+            website=record[2],
+            source=record[3],
+            title=record[4],
+            text=record[5],
+            currencies=DatabaseHandler._json_loads(record[6]),
+            hashtags=DatabaseHandler._json_loads(record[7]),
+            interpretation=Interpretation(record[8]) if record[8] is not None else None,
+            reason=record[9],
+            trade_executed=record[10]
+        )
 
-    def close(self):
-        self.conn.close()
+    # Helper methods for cleaner code: handle json serialization/deserialization.
+    @staticmethod
+    def _json_dumps(data: Optional[object]) -> Optional[str]:
+        return json.dumps(data) if data is not None else None
+
+    @staticmethod
+    def _json_loads(data: Optional[str]) -> Optional[object]:
+        return json.loads(data) if data is not None else None
---
+++ database_handler/sql.py
@@ -0,0 +1,23 @@
+NEWS_TABLE_SQL = """
+    CREATE TABLE IF NOT EXISTS news (
+        url TEXT,
+        date TEXT,
+        website TEXT,
+        source TEXT,
+        title TEXT,
+        text TEXT,
+        currencies TEXT,
+        hashtags TEXT,
+        interpretation TEXT,
+        reason TEXT,
+        trade_executed BOOLEAN
+    )
+"""
+
+INSERT_NEWS_SQL = """
+    INSERT INTO news VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+"""
+
+SELECT_ALL_NEWS_SQL = """
+    SELECT * FROM news
+"""
