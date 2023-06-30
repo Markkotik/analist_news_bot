388292722e29e75192be0a258cb9911dceaa2eba
Mark Settarov <2top4live@gmail.com>
2023-06-30 10:26:21 +0300

    main.py - > rework

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from binance_trader.binance_trader import BinanceTrader
+from database_handler.database_handler import DatabaseHandler
+from news_analyser.news_analyser import NewsAnalyser
+from news_fetcher.news_fetcher import NewsFetcher
+from bot_controller import BotController
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def main():
+    # Инициализируйте все компоненты бота
+    news_fetcher = NewsFetcher()
+    news_analyser = NewsAnalyser()
+    binance_trader = BinanceTrader()
+    db_handler = DatabaseHandler()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Создайте контроллер бота и передайте ему все компоненты
+    bot_controller = BotController(
+        news_fetcher=news_fetcher,
+        news_analyser=news_analyser,
+        binance_trader=binance_trader,
+        db_handler=db_handler,
+    )
+
+    # Запустите бота
+    bot_controller.run()
+
+
+if __name__ == "__main__":
+    main()
