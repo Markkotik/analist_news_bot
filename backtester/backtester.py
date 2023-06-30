52aa97cc39107c04432d99d309f8f5973452e65d
Mark Settarov <2top4live@gmail.com>
2023-06-30 11:23:25 +0300

    backtester.py add

---
+++ backtester/backtester.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+from database_handler.database_handler import DatabaseHandler
+from news_analyser.news_analyser import NewsAnalyser
+from news_fetcher.news_fetcher import NewsFetcher
+
+
+@dataclass
+class BotController:
+    news_fetcher: NewsFetcher
+    news_analyser: NewsAnalyser
+    db_handler: DatabaseHandler(db_name='backtest')
+
+    def fetch_and_analyse(self):
+        news = self.news_fetcher.get_latest_news()
+        analysis = self.news_analyser.analyse_news(news)
+        self.db_handler.save_news(analysis)
+
+    def run(self):
+        while True:
+            self.fetch_and_analyse()
+
