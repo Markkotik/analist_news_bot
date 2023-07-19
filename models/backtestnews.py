0939c0479c90db9b911b27f3ec80f4e9321508a6
Mark Settarov <2top4live@gmail.com>
2023-07-19 13:28:23 +0300

    backtestnews.py -> add from_news

--- models/backtestnews.py
+++ models/backtestnews.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from enum import Enum
-from typing import List, Dict
+from typing import Dict
 
 from models.news import News
 
@@ -24,3 +24,20 @@ class CandleData:
 @dataclass
 class BacktestNews(News):
     candle_data: Dict[CandleDuration, CandleData] = field(default_factory=dict)
+
+    @staticmethod
+    def from_news(news):
+        return BacktestNews(
+            url=news.url,
+            date=news.date,
+            website=news.website,
+            source=news.source,
+            title=news.title,
+            asset=news.asset,
+            text=news.text,
+            currencies=news.currencies,
+            hashtags=news.hashtags,
+            interpretation=news.interpretation,
+            reason=news.reason,
+            trade_executed=news.trade_executed,
+        )
