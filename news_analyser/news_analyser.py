42e124edb48d382c630b540e97b88165a4e4fd05
Mark Settarov <2top4live@gmail.com>
2023-07-19 12:59:04 +0300

    add asset to news

--- news_analyser/news_analyser.py
+++ news_analyser/news_analyser.py
@@ -1,5 +1,7 @@
 import openai
 from typing import List, Tuple, Optional
+
+from binance_trader.binancechecker import BinanceChecker
 from config import OPENAI_API_KEY, PROMT_ANALYSIS_TITLES
 from models.news import News, Interpretation
 
@@ -8,12 +10,14 @@ class NewsAnalyser:
     def __init__(self):
         openai.api_key = OPENAI_API_KEY
         self.prompt = PROMT_ANALYSIS_TITLES
+        self.binance_checker = BinanceChecker()
 
     def analyse_news(self, news_list: List[News]) -> List[News]:
         for news in news_list:
             interpretation, reason = self.get_interpretation_and_reason(news.title)
             news.interpretation = Interpretation(interpretation.upper())
             news.reason = reason
+            news.asset = self.binance_checker.get_tradable_asset(news.currencies)
 
         return news_list
 
